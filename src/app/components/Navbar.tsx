f789113488fb89b2245843ea377c8b0a4a2c041e
MatthewIczkowski <iczkom13@gmail.com>
2025-09-09 20:15:30 -0600

    styling updates

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,15 +6,15 @@ const navbar = () => {
   return (
     <nav>
         <ul className="px-4 py-4 flex list-none m-0 gap-6 items-center">
-            <li className="text-2xl font-bold"> <Image src={logo} alt="logo" width={75} height={75} /></li>
-            <li className="text-xl font-semibold text-gray-500 hover:text-black hover:underline hover:underline-offset-8 hover:decoration-gray-500 hover:decoration-3">Schedule</li>
-            <li className="text-xl font-semibold text-gray-500 hover:text-black hover:underline hover:underline-offset-8 hover:decoration-gray-500 hover:decoration-3">Teams</li>
-            <li className="text-xl font-semibold text-gray-500 hover:text-black hover:underline hover:underline-offset-8 hover:decoration-gray-500 hover:decoration-3">Matches</li>
+            <li> <Image src={logo} alt="logo" width={75} height={75} /></li>
+            <li className="text-md md:text-xl font-semibold text-gray-500 hover:text-black hover:underline hover:underline-offset-8 hover:decoration-gray-500 hover:decoration-3">Schedule</li>
+            <li className="text-md md:text-xl font-semibold text-gray-500 hover:text-black hover:underline hover:underline-offset-8 hover:decoration-gray-500 hover:decoration-3">Teams</li>
+            <li className="text-md md:text-xl font-semibold text-gray-500 hover:text-black hover:underline hover:underline-offset-8 hover:decoration-gray-500 hover:decoration-3">Matches</li>
             {/* <li className="text-xl">Scoring</li> */}
         </ul>
         <div className="px-8 py-4 grid grid-cols-1 md:grid-cols-2 bg-gray-200"> 
-            <div className="flex font-bold md:justify-start">Breckenridge Cup</div>
-            <div className="flex md:justify-end"> Breckenridge, CO | October 2-4, 2025</div>
+            <div className="flex font-bold text-sm md:justify-start">Breckenridge Cup</div>
+            <div className="flex text-sm md:justify-end"> Breckenridge, CO | October 2-4, 2025</div>
 
         </div>
     </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import logo from '../../public/town.jpg'
+import logo from '../../public/Breckenridge-Golf.jpg'
 
 export default function Home() {
   return (
@@ -9,10 +9,10 @@ export default function Home() {
           alt="logo" 
           width={1920} 
           height={1080}
-          className="w-full h-auto"
+          className="hidden md:block md:w-full md:h-auto"
         />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 text-black p-6 rounded-lg">
-          <p className="text-lg text-center mt-2">More info coming soon!</p>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 text-black p-2 md:p-4 rounded-lg">
+          <p className="text-sm md:text-lg text-center mt-2">More info coming soon!</p>
         </div>
     </div>
   );
