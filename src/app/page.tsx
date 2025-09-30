924fc638d5854901deedfa63146d3865bda457d9
MatthewIczkowski <iczkom13@gmail.com>
2025-09-30 15:39:14 -0600

    shadcn updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 import Image from 'next/image'
 import logo from '../../public/Breckenridge-Golf.jpg'
-import CountdownTimer from './components/CountdownTimer'
+import CountdownTimer from '../components/CountdownTimer'
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center overflow-hidden">
-        <Image 
-          src={logo} 
-          alt="logo" 
-          width={1920} 
-          height={1080}
-          className="w-full h-screen object-cover object-center"
-        />
-        <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300/90 text-black p-4 md:p-6 rounded-lg">
-          <CountdownTimer />
-        </div>
+      <Image 
+        src={logo} 
+        alt="logo" 
+        width={1920} 
+        height={1080}
+        className="w-full h-screen object-cover object-center"
+      />
+      <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300/90 text-black p-4 md:p-6 rounded-lg">
+        <CountdownTimer />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import logo from '../../../public/breck-cup-logo.png'
+import logo from '../../public/breck-cup-logo.png'
 
 const Navbar = () => {
   const pathname = usePathname()
